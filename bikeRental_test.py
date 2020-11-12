8449d3d903fdc1e3e68ee59ebee49fcd448a933a
99002630 <71540838+99002630@users.noreply.github.com>
2020-11-12 13:21:23 +0530

    Update bikeRental_test.py

--- bikeRental_test.py
+++ bikeRental_test.py
@@ -5,7 +5,12 @@ from bikeRental import BikeRental, Customer
 
 class BikeRentalTest(unittest.TestCase):
 
-   
+    def test_Bike_Rental_diplays_correct_stock(self):
+
+        shop1 = BikeRental()
+        shop2 = BikeRental(10)
+        self.assertEqual(shop1.displaystock(), 0)
+        self.assertEqual(shop2.displaystock(), 10)
         
     def test_rentBikeOnHourlyBasis_for_negative_number_of_bikes(self):
         shop = BikeRental(10)
